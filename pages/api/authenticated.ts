fcba64259d4e86c7440c52a834a27cc7cb455acc
unknown <maheryrandrianirinadev@gmail.com>
2025-03-31 14:17:31 +0300

    fix: Fix pull conflict

--- pages/api/authenticated.ts
+++ pages/api/authenticated.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { getSession } from "@/lib/index"
 import type { NextApiRequest, NextApiResponse } from "next"
 
@@ -6,14 +5,6 @@ import type { NextApiRequest, NextApiResponse } from "next"
 export async function getServerSideProps({req, res}: {req: NextApiRequest, res: NextApiResponse}){
     const session = await getSession(req, res)
     if(session?.user){
-=======
-import type { NextResponse } from "next/server"
-import { RequestWithSession } from "../../types/session"
-
-
-export async function getServerSideProps({req, res}: {req: RequestWithSession, res: NextResponse}){
-    if(req.session?.user){
->>>>>>> 929a86fddfdd0fe7cf92aaf12ba32d67f3fdb2d2
         return {
             props: {},
             redirect: {
